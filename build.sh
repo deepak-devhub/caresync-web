646fa8b9e83878cb898786457b0984cd5ecc0eb8
Deepak VK <159251570+deepak-devhub@users.noreply.github.com>
2025-12-03 00:12:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-python manage.py collectstatic --noinput
+set -e
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear --no-input
+echo "Static files collected successfully"
