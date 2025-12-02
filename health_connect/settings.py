690c7647cbe6e16c04501a5f280394c4578dc95d
Deepak VK <159251570+deepak-devhub@users.noreply.github.com>
2025-12-03 00:33:32 +0530

    Update settings.py

--- health_connect/settings.py
+++ health_connect/settings.py
@@ -144,8 +144,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'Healthify', 'static')
-STATICFILES_DIRS = []
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Healthify', 'static'),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
