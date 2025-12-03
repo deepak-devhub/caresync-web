0ebaf97e1883fc2b98be0caab96d221b9f013b37
Deepak VK <159251570+deepak-devhub@users.noreply.github.com>
2025-12-03 09:55:15 +0530

    Update wsgi.py

--- health_connect/wsgi.py
+++ health_connect/wsgi.py
@@ -1,16 +1,23 @@
-"""
-WSGI config for health_connect project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+from django.core.management import call_command
+
+# Add project directory to path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'health_connect.settings')
+
+# Collect static files on first import (Vercel deployment)
+if os.environ.get('VERCEL'):
+    print("Running on Vercel - collecting static files...")
+    import django
+    django.setup()
+    call_command('collectstatic', '--noinput', '--clear')
+    print("Static files collected!")
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'health_connect.settings')
 
 app = get_wsgi_application()
